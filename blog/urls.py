f936ff61a1ef61ef1f8c95891a25884f915cf140
gaspar91 <andreiafg.91@gmail.com>
2021-01-27 23:24:04 +0000

    added styles to add comment seccion in post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.posts, name='posts'),
     path('posts/<slug:slug>/', views.PostDetail, name='post_detail'),
+    # path('add_post/', views.add_post, name='add_post'),
 ]
