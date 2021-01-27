afc35caa2f168952cb52a1d692e5b2deb1eaf7a1
gaspar91 <andreiafg.91@gmail.com>
2021-01-27 02:14:39 +0000

    fixed blog template page, models and urls

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
-from django.views import generic
 from .models import Post
+from django.shortcuts import render
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'posts.html'
+def posts(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
 
-
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+    return render(request, 'blog/posts.html', context)
