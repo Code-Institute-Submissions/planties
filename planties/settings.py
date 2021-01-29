31caf3976b8747fd46c5e17202114e2aa5d2e61e
gaspar91 <andreiafg.91@gmail.com>
2021-01-29 11:48:34 +0000

    loadded categories and data for deployment

--- planties/settings.py
+++ planties/settings.py
@@ -121,16 +121,13 @@ WSGI_APPLICATION = 'planties.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse('postgres://ohkwnmlgadplpd:e2fcb20e0d92e30243fb9afd2679fddd36a1f77f22a6a015b1282733e8d0e49f@ec2-52-212-157-46.eu-west-1.compute.amazonaws.com:5432/da779bse2ba8b0')
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
